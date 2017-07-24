fd3e6af6e00f65ff5e391d970f815865fb65d59c
ivarus <ivarbjorling@hotmail.com>
2017-07-24 13:40:31 +0200

    Init OSStructured

--- Assets/Resources/OSStuctrue/OSStuctrueMain.cs
+++ Assets/Resources/OSStuctrue/OSStuctrueMain.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class OSStuctrueMain : MonoBehaviour
 {
-    const int width = 8192;
-    const int height = 4096;
+    const int width = 1024; // TMP 8192
+    const int height = 4096; // TMP 4096
     const float spacing = 0.05f;
 
     ComputeBuffer mPositionBuffer = null;
     ComputeBuffer mArgsBuffer = null;
 
+    ComputeShader mComputeShader = null;
+    ComputeBuffer mVertexBuffer = null;
+
     Material mRenderMaterial = null;
 
 	void Start ()
     {
-        Camera.main.transform.position = new Vector3(width / 2.0f * spacing, height / 2.0f * spacing, -150);
+        Camera.main.transform.position = new Vector3(width / 2.0f * spacing, height / 2.0f * spacing, -50); // TMP -150
 
         mPositionBuffer = new ComputeBuffer(width * height, sizeof(float) * 4, ComputeBufferType.Default);
         mArgsBuffer = new ComputeBuffer(1, sizeof(int) * 4, ComputeBufferType.IndirectArguments);
@@ -23,6 +26,11 @@ public class OSStuctrueMain : MonoBehaviour
         Shader renderShader = Resources.Load<Shader>("OSStuctrue/OSStuctrueRenderShader");
         Debug.Assert(renderShader, "Failed loading render shader.");
         mRenderMaterial = new Material(renderShader);
+
+        mComputeShader = Resources.Load<ComputeShader>("OSStuctrue/OSStuctrueComputeShader");
+        Debug.Assert(mComputeShader, "Failed loading compute shader.");
+
+        mVertexBuffer = new ComputeBuffer(width * height * 6, sizeof(float) * 4);
 
         Vector4[] positionArray = new Vector4[width * height];
         for (int i = 0; i < width * height; ++i)
@@ -36,14 +44,18 @@ public class OSStuctrueMain : MonoBehaviour
 	
 	void Update ()
     {
+        mComputeShader.SetBuffer(0, "gPosition", mPositionBuffer);
+        mComputeShader.SetBuffer(0, "gVertexBuffer", mVertexBuffer);
+        mComputeShader.SetInt("gCount", width * height);
 
+        mComputeShader.Dispatch(0, (int)Mathf.Ceil(width * height / 64.0f), 1, 1);
     }
 
     void OnRenderObject()
     {
         mRenderMaterial.SetPass(0);
 
-        mRenderMaterial.SetBuffer("gPosition", mPositionBuffer);
+        mRenderMaterial.SetBuffer("gVertexBuffer", mVertexBuffer);
 
         Graphics.DrawProceduralIndirect(MeshTopology.Triangles, mArgsBuffer);
     }
@@ -52,6 +64,8 @@ public class OSStuctrueMain : MonoBehaviour
     {
         mPositionBuffer.Release();
         mArgsBuffer.Release();
+
+        mVertexBuffer.Release();
     }
 
 }
--- Assets/Resources/OSStuctrue/OSStuctrueRenderShader.shader
+++ Assets/Resources/OSStuctrue/OSStuctrueRenderShader.shader
@@ -19,7 +19,7 @@
 			#pragma vertex vert
 			#pragma fragment frag
 
-			StructuredBuffer<float4> gPosition;
+			StructuredBuffer<float4> gVertexBuffer;
 
 			// ---
 
@@ -33,24 +33,13 @@
 			{
                 VS_OUTPUT output;
 
-                float3 lensUp = UNITY_MATRIX_IT_MV[1].xyz;
+                float3 vPosition = gVertexBuffer[iID * 6 + vID];
 
-                float3 pPosition = gPosition[iID].xyz;
-                float2 pScale = float2(0.1f, 0.1f);
-
-                float3 pForward = normalize(_WorldSpaceCameraPos - pPosition);
-                float3 pRight = cross(pForward, lensUp);
-                float3 pUp = cross(pRight, pForward);
-
+                output.svPosition = UnityObjectToClipPos(float4(vPosition, 1));
 
                 float x = vID == 0 || vID == 1 || vID == 3;
                 float y = vID == 0 || vID == 2 || vID == 5;
-
-                float3 vPosition = pPosition + pRight * ((x * 2.f - 1.f) * pScale.x) + pUp * ((y * 2.f - 1.f) * pScale.y);
-
-                output.svPosition = UnityObjectToClipPos(float4(vPosition, 1));
                 output.uv = float2(x, 1.0f - y);
-
 
 				return output;
 			}
